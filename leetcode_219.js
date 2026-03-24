d2e4bb5dc9ec2e488c399beb03de8d9f62e8e682
ekrmabd <ekramhossainekram28@gmail.com>
2026-03-24 17:58:23 +0600

    leetcode 1, 53. 121, 153, 189, 283 solved

---
+++ leetcode_219.js
@@ -0,0 +1,13 @@
+//219. Contains Duplicate II
+
+var containsNearbyDuplicate = function(nums, k) {
+    //
+};
+
+containsNearbyDuplicate([1,2,3,1],3);
+//containsNearbyDuplicate([1,0,1,1],1);
+//containsNearbyDuplicate([1,2,3,1,2,3],2);
+
+//console.log(containsNearbyDuplicate([1,2,3,1],3));
+//console.log(containsNearbyDuplicate([1,0,1,1],1));
+//console.log(containsNearbyDuplicate([1,2,3,1,2,3],2));
