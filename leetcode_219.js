4183de49efc3bb77aecfe788145376086e0b3cbe
ekrmabd <ekramhossainekram28@gmail.com>
2026-03-25 11:54:28 +0600

    leetcode 219 solved

--- leetcode_219.js
+++ leetcode_219.js
@@ -1,13 +1,21 @@
 //219. Contains Duplicate II
 
 var containsNearbyDuplicate = function(nums, k) {
-    //
+    let map = new Map();
+    let diff;
+    for(let i = 0; i < nums.length; i++)
+    {   
+        if(map.has(nums[i])){
+            let lastIdx = map.get(nums[i]);
+            diff = i - lastIdx;
+            if(diff <= k) return true;
+        }
+        map.set(nums[i],i);
+    }
+    return false;    
 };
 
-containsNearbyDuplicate([1,2,3,1],3);
-//containsNearbyDuplicate([1,0,1,1],1);
-//containsNearbyDuplicate([1,2,3,1,2,3],2);
 
-//console.log(containsNearbyDuplicate([1,2,3,1],3));
-//console.log(containsNearbyDuplicate([1,0,1,1],1));
-//console.log(containsNearbyDuplicate([1,2,3,1,2,3],2));
+console.log(containsNearbyDuplicate([1,2,3,1],3));
+console.log(containsNearbyDuplicate([1,0,1,1],1));
+console.log(containsNearbyDuplicate([1,2,3,1,2,3],2));
